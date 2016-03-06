527a48622e0cf63b5905925296a1e2dd1778f4ee
Manu Dilip Shah <manu@appcito.net>
2016-03-06 10:18:59 +0530

    Matrix Chain Multiplication

--- MatrixChainMultiplication/main.cpp
+++ MatrixChainMultiplication/main.cpp
@@ -7,9 +7,58 @@
 //
 
 #include <iostream>
+#include <climits>
+using namespace std;
+
+int getMinMultiplications (int * P, int n) {
+    
+    int M[n][n];
+    
+    for (int i = 1;i<n;i++){
+        M[i][i] = 0;
+    }
+    
+    //chain length
+    for (int l=2; l <= n;l++) {
+        for (int i = 1; i <= n - l +  1; i++) {
+            int j = l+i-1;
+            
+            M[i][j] = INT_MAX;
+            for (int k = i; k <= j-1; k++) {
+                
+                int thiscost = M[i][k] + M[k+1][j] + P[i-1]*P[k]*P[j];
+                
+                if (thiscost < M[i][j]) {
+                    M[i][j] = thiscost;
+                }
+            }
+        }
+    }
+    
+    for (int i = 0;i<n;i++) {
+        for (int j = 0;j<n; j++) {
+            cout<<M[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    
+    return M[1][n-1];
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int n;
+    cin>>n;
+    
+    int * P =  new int [n];
+    
+    for (int i = 0; i < n; i++) {
+        cin>>P[i];
+    }
+    
+    int m = getMinMultiplications (P, n);
+    
+    cout<<m;
+    
     return 0;
 }
