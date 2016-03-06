8a85d4a276622a3a5f6c2199c9720be4ecbb4e4d
Manu Dilip Shah <manu@appcito.net>
2016-03-06 09:49:12 +0530

    Initial Commit

---
+++ MatrixChainMultiplication/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MatrixChainMultiplication
+//
+//  Created by Manu Shah on 06/03/16.
+//  Copyright © 2016 Manu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
